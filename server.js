ef06c72dea295968981e061bf6a10f0a6e31f9ea
nida0000 <nidaiftikhar98@gmail.com>
2025-05-01 17:08:59 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -25,4 +25,5 @@ app.use('/api/skin',skinRoutes)
 app.use('/api/user',userRoutes)
 app.use("/api/body-shape", bodyRoutes);
 
-export default app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
